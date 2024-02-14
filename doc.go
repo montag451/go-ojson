bac9bec9bd7e729ac1fe725b8aec3b3c3fe05c9c
montag451 <montag451@laposte.net>
2024-02-14 13:58:13 +0100

    Improve package documentation

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package ojson implements several data types that can be used to
+// Package ojson provides several data types that can be used to
 // encode and decode JSON while preserving object keys order. The name
 // "ojson" stands for "Ordered JSON".
 package ojson
